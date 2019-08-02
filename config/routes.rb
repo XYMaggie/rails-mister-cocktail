0bfe44135cd8dcd02c4f91a32fbf594fb962b316
Yuman Xia <yumanxia@foxmail.com>
2019-08-02 16:15:02 +0800

    added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :destroy], shallow: true
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
 end
